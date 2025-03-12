a45323b3294b8bbbbeeb9675232ed2904c77e15e
arg054 <maxim.golovenkov@gmail.com>
2025-03-12 19:29:09 +0100

    implementing logic and unit tests to split by delimiter

---
+++ src/test_splitnodes.py
@@ -0,0 +1,16 @@
+import unittest
+
+from splitnodes import split_nodes_delimiter
+from textnode import TextNode, TextType
+
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        old_nodes = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ]
+        self.assertEqual(new_nodes, old_nodes)
