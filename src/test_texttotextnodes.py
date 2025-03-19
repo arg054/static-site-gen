557d0f4822cc5f490f91d827253da7c3ff1d847f
arg054 <maxim.golovenkov@gmail.com>
2025-03-19 08:53:22 +0100

    implementing texttotextnodes and its unit tests

---
+++ src/test_texttotextnodes.py
@@ -0,0 +1,28 @@
+import unittest
+
+from textnode import TextNode, TextType
+from texttotextnodes import text_to_textnodes
+
+
+class TestTextToTextNodes(unittest.TestCase):
+    def test1(self):
+        matches = text_to_textnodes(
+            "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        )
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode(
+                    "obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"
+                ),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            matches,
+        )
---
+++ src/texttotextnodes.py
@@ -0,0 +1,14 @@
+from textnode import TextNode, TextType
+from splitnodes import split_nodes_delimiter, split_nodes_image, split_nodes_link
+
+
+def text_to_textnodes(text):
+    initial_node = TextNode(text, TextType.TEXT)
+    new_nodes = [initial_node]
+    node_def = {"**": TextType.BOLD, "_": TextType.ITALIC, "`": TextType.CODE}
+
+    for markdown_def, node_type in node_def.items():
+        new_nodes = split_nodes_delimiter(new_nodes, markdown_def, node_type)
+    new_nodes = split_nodes_image(new_nodes)
+    new_nodes = split_nodes_link(new_nodes)
+    return new_nodes
